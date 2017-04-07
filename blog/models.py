af942abef1e040099347ece270e0458bd8b69d96
zhou <zhou2808972009@outlook.com>
2017-04-07 21:43:12 +0800

    完成基本功能

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import  reverse
 from django.utils.six import python_2_unicode_compatible
 
 @python_2_unicode_compatible
@@ -37,4 +38,5 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-# Create your models here.
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
