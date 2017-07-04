aaa9780fe39554860a76697515009db2a199fdb4
zhou <2808972009@qq.com>
2017-07-04 18:41:52 +0800

    add views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Post,Category
 import markdown
+from django.views.generic import ListView, DetailView
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger#主页分页用
-
+from django.db.models import Q
 def home(request):
     posts=Post.objects.all().order_by('-created_time')
     paginator=Paginator(posts,5)
@@ -21,13 +22,14 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    post.body = markdown.markdown(post.body,
-                                  extensions=[
-                                      'markdown.extensions.extra',
-                                      'markdown.extensions.codehilite',
-                                      'markdown.extensions.toc',
-                                  ])
-    return render(request, 'blog/detail.html', context={'post': post})
+    post.increase_views()
+    md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            'markdown.extensions.toc',
+        ])
+    post.body = md.convert(post.body)
+    return render(request, 'blog/detail.html', context={'post': post,})
 
 def archives(request,year,month):
     post_list=Post.objects.filter(created_time__year=year,created_time__month=month)
@@ -37,3 +39,35 @@ def category(request,pk):
     cate=get_object_or_404(Category,pk=pk)
     post_list=Post.objects.filter(category=cate)
     return render(request,'blog/index.html',context={'post_list':post_list})
+
+def search(resuest):
+    q=request.GET.get('q')
+    error_msg=''
+    if not q:
+        error_msg="请输出关键词"
+        return render(request, 'blog/index.html', {'error_msg': error_msg})
+    post_list=Post.objects.filter(Q(title__icontains=q) | Q(body__icontains=q))
+    return render(request,'blog/index.html', {'error_msg': error_msg,
+                                               'post_list': post_list})
+class PostDetailView(DetailView):
+    # 这些属性的含义和 ListView 是一样的
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
+
+    def get(self, request, *args, **kwargs):
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+        self.object.increase_views()
+        return response
+    def get_object(self, queryset=None):
+        post = super(PostDetailView, self).get_object(queryset=None)
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        md = markdown.Markdown(extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+            'markdown.extensions.toc',
+        ])
+        post.body = md.convert(post.body)
+        post.toc = md.toc
+        return post
+
--- pythonblog/settings.py
+++ pythonblog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '9578*d0$qq$qbo_(1ee!_p9*-+^khd3i4zb2b0w_f&&mq^w@d+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 
-ALLOWED_HOSTS = ['23.106.155.192','www.haibin.website','*']
+ALLOWED_HOSTS = ['23.106.155.192','www.haibin.website','*','haibin.website']
 
 
 # Application definition
