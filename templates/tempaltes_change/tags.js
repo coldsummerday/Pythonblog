19569fafbc7803535642ec9db6236b906cf502b5
coldsummer <zhou2808972009@outlook.com>
2018-01-13 11:02:08 +0800

    add tagCanvas

--- templates/tempaltes_change/tags.js
+++ templates/tempaltes_change/tags.js
@@ -4,13 +4,10 @@
 
 function fssilde() {
     var mheader = document.getElementById('m-header'),
-        menuLeft = document.getElementById('m-nav'),
         mcontainer = document.getElementById('m-container'),
-        mfooter = document.getElementById('m-footer'),
         showLeftPush = document.getElementById('showLeftPush');
     showLeftPush.onclick = function () {
         classie.toggle(mheader, 'm-header-open');
-        classie.toggle(menuLeft, 'm-nav-open');
         classie.toggle(mcontainer, 'm-container-open');
         classie.toggle(mfooter, 'm-footer-open');
         disableOther('showLeftPush');
